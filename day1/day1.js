f8f8bef230488d1e4fbc67173cede724254a08ce
Romain Boudet <romain@billetweb.fr>
2022-12-01 15:20:52 +0100

    Exo 2 done

--- day1/day1.js
+++ day1/day1.js
@@ -17,3 +17,14 @@ console.log(arrayOfSomme);
 // Prendre la valeur max du tableau
 const result = Math.max(...arrayOfSomme)
 console.log(result);
+
+// connaitre le total en calorie des 3 premiers Elfes
+// On trie le tableau, on selectionne seulement les 3 premlier, et on additionne les valeurs 
+
+const result2 = arrayOfSomme.sort((a, b) =>  b - a).slice(0, 3).reduce((acc, curr)=> acc + curr);
+
+console.log(result2);
+
+
+
+
